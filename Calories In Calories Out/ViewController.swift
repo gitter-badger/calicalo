cb16929756b39f99b95e25b7f1948bc9a2fbfde1
Ryan Klein <ryan.klein@outlook.com>
2016-09-18 19:40:20 -0400

    Initial Commit

---
+++ Calories In Calories Out/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calories In Calories Out
+//
+//  Created by Ryan Klein on 9/18/16.
+//  Copyright © 2016 Base11 Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
