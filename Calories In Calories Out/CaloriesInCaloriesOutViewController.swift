517cb6e144592abb4318ba68226e16511b8af455
Ryan Klein <ryan.klein@outlook.com>
2017-02-12 00:59:04 -0500

    Tableview with refresh control

--- Calories In Calories Out/CaloriesInCaloriesOutViewController.swift
+++ Calories In Calories Out/CaloriesInCaloriesOutViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import HealthKit
 
 
-class CaloriesInCaloriesOutViewController : UIViewController{
+class CaloriesInCaloriesOutViewController : UITableViewController{
     
 
     
@@ -42,6 +42,7 @@ class CaloriesInCaloriesOutViewController : UIViewController{
             navigationItem.rightBarButtonItem = UIBarButtonItem(title: "refresh", style: .plain, target: self, action: #selector(refreshTouched))
             
         }
+        
     }
     
         
@@ -54,6 +55,12 @@ class CaloriesInCaloriesOutViewController : UIViewController{
             
         }
     }
+    @IBAction func beginRefresh(_ sender: Any) {
+        
+        if refreshControl?.isRefreshing == true{
+            loadCalories()
+        }
+    }
     
 }
 
@@ -61,6 +68,9 @@ class CaloriesInCaloriesOutViewController : UIViewController{
 
 extension CaloriesInCaloriesOutViewController:CalorieDataLoader{
     func allDone(){
+        if self.refreshControl?.isRefreshing == true{
+            self.refreshControl?.endRefreshing()
+        }
         if let calorieData = calorieData, let restingCalories = calorieData.restingCaloriesAverage , let activeCalories = calorieData.activeCalories, let caloriesConsumed = calorieData.caloriesConsumed, let netCalories = calorieData.netCalories{
             restingCaloriesLabel.text = String(restingCalories)
             activeCaloriesLabel.text = String(activeCalories)
